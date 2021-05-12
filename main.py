9156a73f7018d29748cb05522b823a05a4cb8211
elijah15976 <eren5836@baysidehighschool.org>
2021-05-12 13:10:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import turtle
 import time
 from core_functions import *
 
+turtle.bgcolor("light blue")
+
 bob = turtle.Turtle()
+
+bob.speed(0)
 
 jump(bob, 0, 350)
 bob.right(45)
 
+bob.color("white")
+bob.begin_fill()
 polygon(bob, 4,500, "right")
+bob.end_fill()
 
 #End of Program
 time.sleep(5)
