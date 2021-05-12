ee594517d6566485c567106251f7a1a464ca5784
elijah15976 <eren5836@baysidehighschool.org>
2021-05-12 13:06:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import turtle
+import time
+from core_functions import *
 
 bob = turtle.Turtle()
 
+jump(bob, 0, 350)
+bob.right(45)
+
+polygon(bob, 4,500, "right")
+
+#End of Program
+time.sleep(5)
