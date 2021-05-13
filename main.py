e6536f50db1229910b47ebbacee9d98565917fa5
elijah15976 <eren5836@baysidehighschool.org>
2021-05-13 13:59:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+#Importing Stuff
 import turtle
 import time
+import math
 from core_functions import *
 
+#Background
 turtle.bgcolor("light blue")
 
+#Turtle
 bob = turtle.Turtle()
-
 bob.speed(0)
 
+#Start Position
 jump(bob, 0, 350)
 bob.right(45)
 
-bob.color("white")
+#Sideways Square
+bob.color("black")
+bob.fillcolor("white")
 bob.begin_fill()
-polygon(bob, 4,500, "right")
+polygon(bob, 4, 500, "right")
 bob.end_fill()
+bob.right(45)
+
+#Insides
+bob.forward(math.sqrt(500000))
 
 #End of Program
 time.sleep(5)
